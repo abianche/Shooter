e14516f5fe24d2adadcb23335caa32597a7c62fb
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2023-11-28 23:28:34 +0100

    part: custom signals

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+func _on_gate_player_entered_gate(body: Node2D) -> void:
+	print("player has entered gate")
+	print(body)
+
+
+func _on_player_laser() -> void:
+	print("laser from level")
+
+
+func _on_player_grenade() -> void:
+	print("grenade from level")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+signal laser
+signal grenade
+
+var can_laser: bool = true
+var can_grenade: bool = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	
@@ -7,8 +13,19 @@ func _process(_delta: float) -> void:
 	velocity = direction * 500
 	move_and_slide()
 	
-	if Input.is_action_pressed("primary action"):
-		print("shoot laser")
+	if Input.is_action_pressed("primary action") and can_laser:
+		laser.emit()
+		can_laser = false
+		$TimerLaser.start()
 		
-	if Input.is_action_pressed("secondary action"):
-		print("shoot grenade")
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		grenade.emit()
+		can_grenade = false
+		$TimerGrenade.start();
+
+func _on_timer_laser_timeout() -> void:
+	can_laser = true
+
+
+func _on_timer_grenade_timeout() -> void:
+	can_grenade = true
