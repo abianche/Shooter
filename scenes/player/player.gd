f0532a83cd1635252ae3aadff28013a616dfb2b9
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2023-11-28 20:19:03 +0100

    part: physics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,14 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+extends CharacterBody2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 	
 	if Input.is_action_pressed("primary action"):
 		print("shoot laser")
 		
 	if Input.is_action_pressed("secondary action"):
-		print("shoot granade")
+		print("shoot grenade")
