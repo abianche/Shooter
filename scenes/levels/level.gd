8851b23bfd5eb450b967b00d85dab76f49f31e8b
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2023-11-29 12:16:57 +0100

    part: camera

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
+
 func _on_gate_player_entered_gate(body: Node2D) -> void:
 	print("player has entered gate")
 	print(body)
 
 
-func _on_player_laser() -> void:
-	print("laser from level")
+func _on_player_laser(pos, dir) -> void:
+	var laser = laser_scene.instantiate() as Area2D
+	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(dir.angle()) + 90
+	laser.direction = dir
+	$Projectiles.add_child(laser)
+	
 
-
-func _on_player_grenade() -> void:
-	print("grenade from level")
+func _on_player_grenade(pos, dir) -> void:
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.linear_velocity = dir * grenade.speed
+	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal laser
-signal grenade
+signal laser(pos: Vector2, dir: Vector2)
+signal grenade(pos: Vector2, dir: Vector2)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -13,15 +13,22 @@ func _process(_delta: float) -> void:
 	velocity = direction * 500
 	move_and_slide()
 	
+	look_at(get_global_mouse_position())
+	
+	var dir = (get_global_mouse_position() - position).normalized()
+	
 	if Input.is_action_pressed("primary action") and can_laser:
-		laser.emit()
 		can_laser = false
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers.pick_random() as Marker2D
 		$TimerLaser.start()
+		laser.emit(selected_laser.global_position, dir)
 		
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		grenade.emit()
 		can_grenade = false
+		var pos = $LaserStartPositions.get_children()[0].global_position
 		$TimerGrenade.start();
+		grenade.emit(pos, dir)
 
 func _on_timer_laser_timeout() -> void:
 	can_laser = true
