b91f3a811342b5562489efa96d032cf1505ac828
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2023-11-30 12:27:33 +0100

    part: particles

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -18,6 +18,7 @@ func _process(_delta: float) -> void:
 	var dir = (get_global_mouse_position() - position).normalized()
 	
 	if Input.is_action_pressed("primary action") and can_laser:
+		$GPUParticles2D.emitting = true
 		can_laser = false
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers.pick_random() as Marker2D
