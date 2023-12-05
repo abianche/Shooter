7f6b90f8a2035a6ec57240ea656d22fc878650c6
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2023-12-05 22:44:18 +0100

    part: prettier transitions

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,12 +1,8 @@
 extends LevelParent
 
-@export var outside_level_scene: PackedScene
 
 func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	call_deferred("deferred_scene_change")
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
 
-
-func deferred_scene_change():
-	get_tree().change_scene_to_packed(outside_level_scene)
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,14 +1,10 @@
 extends LevelParent
 
 
-
 func _on_gate_player_entered_gate(_body: Node2D) -> void:
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	call_deferred("deferred_scene_change")
-
-func deferred_scene_change():
-	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	TransitionLayer.change_scene("res://scenes/levels/inside.tscn")
 
 func _on_house_player_entered() -> void:
 	var tween = get_tree().create_tween()
@@ -16,7 +12,6 @@ func _on_house_player_entered() -> void:
 	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
 	#tween.tween_property($Player, "modulate:a", 0, 2).from(0.5)
 	
-
 func _on_house_player_exited() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 2)
